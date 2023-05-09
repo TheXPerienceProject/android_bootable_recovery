1ae8434bcb71e122513bdf9cbcba406bb27e560b
Jackeagle <jackeagle102@gmail.com>
2023-05-09 13:53:57 -0400

    recovery: Remove unwanted timestamp var
    Change-Id: I8376e80d11b5ba208288aeef4a4bb0007c0ae72b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- recovery.cpp
+++ recovery.cpp
@@ -851,8 +851,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.bliss.build.version", "(unknown)") +
-        " (" + ver_date + ")",
+    "Version " + android::base::GetProperty("ro.bliss.build", "(unknown)"),
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
