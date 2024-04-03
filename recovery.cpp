b7fac41c930c1eb9c0af7e915ec895d95612b666
klozz <carlosj@klozz.dev>
2024-04-03 03:58:35 -0600

    fix version
    Signed-off-by: klozz <carlosj@klozz.dev>

--- recovery.cpp
+++ recovery.cpp
@@ -847,14 +847,14 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
 
   // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
-  // DERP_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.derpfest.version", "");
+  // XPERIENCE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
+  std::string ver = android::base::GetProperty("ro.xpe.version", "");
   std::smatch ver_date_match;
   std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.derpfest.build_date", "(unknown)") +
+    "Version " + android::base::GetProperty("ro.xpe.build.version", "(unknown)") +
         " (" + ver_date + ")",
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
