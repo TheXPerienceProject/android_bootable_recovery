78d0b5e96251952d11775ceed46132c49629dea8
Jis G Jacob <studiokeys@blissroms.org>
2024-08-10 05:13:41 -0400

    recovery: Versioning Updates
    Change-Id: I5f6d7a4d33dd8d9a33071fe512f0399d6af95b14
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- recovery.cpp
+++ recovery.cpp
@@ -851,7 +851,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.bliss.build", "(unknown)"),
+    "Bliss Version - " + android::base::GetProperty("ro.bliss.version", "(unknown)"),
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
